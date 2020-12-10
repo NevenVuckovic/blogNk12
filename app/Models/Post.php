6b6f56f588056b976ee183d5a5d5ab3a020163ff
Drago Stevanovic <drago.s@vivifyideas.com>
2020-12-10 20:39:52 +0100

    Migration, model, All posts and single post pages

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+  use HasFactory;
+
+  public static function published() {
+    return self::where('is_published', true);
+  }
+
+  public static function unpublished() {
+    return self::where('is_published', false);
+  }
+
+  public function getSomePostData() {
+    return $this->title . ' ovo je iz metode';
+  }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// class QueryBuilder {
+//   private $fields = [];
+//   private $conditions = [];
+//   private $order = null;
+
+//   public function __construct() {}
+
+//   public function where($condition) {
+//     $this->conditions[] = $condition;
+//     return $this;
+//   }
+
+//   public function orderBy($order) {
+//     $this->order = $order;
+//     return $this;
+//   }
+
+
+//   public function get() {
+//     $sql ='select * from table where dslfhsljfklvzhfgdxkcj';
+//     $dbConnection->execute($sql);
+//     new Post($row)
+
+//   }
+
+// }
+
+
 Route::get('/', function () {
-    return view('welcome');
+  // $posts = Post::orderBy('id', 'desc')->where('is_published',1)->get();
+  $posts = Post::published()
+    ->orderBy('id', 'desc')
+    ->get();
+
+  // select content from posts
+  //  where is_published=1;
+  // return view('posts', [
+  //   'posts' => $posts,
+  // ]);
+  return view('posts', compact('posts'));
 });
+
+Route::get('/posts/{post}', function (Post $post) {
+  // $post = Post::findOrFail($id); // where('id', $id)->first()
+  // select * from users where email = $email;
+  // User::find('email', $email)
+  // User::where('email', $email)->first(); {} | null
+  return view('post', compact('post'));
+  // pronadjemo post sa IDem $id
+  // vratimo view single post
+})->name('posts.single');
